580b1015cf4e7827e44e9a789d2a731a7ba34951
theKorax <zikoramadin@gmail.com>
2023-09-06 10:27:29 +0100

    Updated SpawnManager.cs

--- PlayFetch/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ PlayFetch/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -26,7 +26,7 @@ public class SpawnManagerX : MonoBehaviour
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[Random.Range(0, ballPrefabs.Length)], spawnPos, ballPrefabs[Random.Range(0, ballPrefabs.Length)].transform.rotation);
     }
 
 }
